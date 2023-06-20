f81d7c9342d1dd7bf4d67e060ce9fae309b31999
Diego Lucio D'Onofrio <diego.lucio.donofrio@gmail.com>
2023-06-20 13:30:57 +0200

    Update calibration.ino

--- FLUME/examples/calibration/calibration.ino
+++ FLUME/examples/calibration/calibration.ino
@@ -1,20 +1,18 @@
 #include <FLUME.h>
 
-const int FLOW_METER_PIN = 2;  // Pin conectado al caudalímetro
-FLUME flume(FLOW_METER_PIN);   // Crea una instancia del objeto FLUME
+const int FLOW_METER_PIN = 2;  
+FLUME flume(FLOW_METER_PIN);   
 
 void setup() {
-  Serial.begin(9600); // Inicia la comunicación serial a 9600 bps
+  Serial.begin(9600); 
 }
 
 void loop() {
   delay(1000);
   
-  // Obtiene los ticks acumulados y el caudal
   long accumulatedTicks = flume.getAccumulatedTicks();
   float flowRate = flume.getFlowRate();
 
-  // Muestra los ticks acumulados y el caudal
   Serial.print("Accumulated Ticks: ");
   Serial.println(accumulatedTicks);
   Serial.print("Flow Rate: ");
